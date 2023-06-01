cd250715568bb2dba1ab45943ab6d0af177da226
Seongeun Choi <Seenz.choi@gmail.com>
2023-06-01 15:38:46 +0900

    [refactoring] 39p

--- TDD_GTest/gtest_02.cpp
+++ TDD_GTest/gtest_02.cpp
@@ -1,15 +1,19 @@
 #include "pch.h"
 #include "../TDD/test_02.cpp"
 
-
-TEST(PrimeFactorTC, Of1) {
+class PrimeFixture : public testing::Test
+{
+public:
 	PrimeFactor prime_factor;
-	std::vector<int> expected = {};
+	std::vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactorTC, Of2) {
-	PrimeFactor prime_factor;
-	std::vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
