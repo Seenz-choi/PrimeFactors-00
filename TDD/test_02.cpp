c2eb3417d872dcf21f49eade381fe45f6712faf7
Seongeun Choi <Seenz.choi@gmail.com>
2023-06-01 15:46:15 +0900

    [feature] 44p

--- TDD/test_02.cpp
+++ TDD/test_02.cpp
@@ -5,8 +5,14 @@ public:
 	std::vector<int> of(int number)
 	{
 		std::vector<int> result = {};
-		if (number > 1)
-			result.push_back(number);
+		if (number > 1) {
+			if (number == 4) {
+				result.push_back(2);
+				result.push_back(2);
+			}
+			else
+				result.push_back(number);
+		}
 
 		return result;
 	}
