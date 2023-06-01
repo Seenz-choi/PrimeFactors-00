b5f9ddbd2f9f5fe84ef09785aeb05e7a1d2663fc
Seongeun Choi <Seenz.choi@gmail.com>
2023-06-01 15:34:21 +0900

    [feature] 37p

--- TDD_GTest/gtest_02.cpp
+++ TDD_GTest/gtest_02.cpp
@@ -7,3 +7,9 @@ TEST(PrimeFactorTC, Of1) {
 	std::vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactorTC, Of2) {
+	PrimeFactor prime_factor;
+	std::vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
